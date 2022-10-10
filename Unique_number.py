caa5447875627cf36958bbe54891f5106e0afdd3
Swathivimalag555 <97424553+Swathivimalag555@users.noreply.github.com>
2022-10-10 22:56:05 +0530

    n=int(input()) m=str(n) k='' for i in m:     if(i in k):         print("Not Unique Number")         break     else:         k+=i if(len(k)==len(m)):     print("Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,18 +1,11 @@
-s=input()
-v=0
-v=len(s)
-for i in range(v):
-    x=0
-    for j in range(v):
-        if(i!=j):
-            if(s[i]==s[j]):
-                x+=1
-                break
-    if(x>0):
+n=int(input())
+m=str(n)
+k=''
+for i in m:
+    if(i in k):
+        print("Not Unique Number")
         break
     else:
-        i+=1
-if(x==0):
-    print('Unique Number')
-else:
-    print('Not Unique Number')
+        k+=i
+if(len(k)==len(m)):
+    print("Unique Number")
