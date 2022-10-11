32a86594767c53b5b734e15664579a5b104b85e9
Swathivimalag555 <97424553+Swathivimalag555@users.noreply.github.com>
2022-10-11 10:00:51 +0530

    def is_disarium(num):     temp = 0     for i in range(len(str(num))):         temp += int(str(num)[i]) ** (i + 1)     return temp == num n=int(input()) print(is_disarium(n))

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,18 +1,7 @@
+def is_disarium(num):
+    temp = 0
+    for i in range(len(str(num))):
+        temp += int(str(num)[i]) ** (i + 1)
+    return temp == num
 n=int(input())
-c=0
-s=0
-temp=n
-while n>0:
-    r=n%10
-    n//=10
-    c+=1
-n=temp
-while n>0:
-    r=n%10
-    s=s+pow(r,c)
-    n=n//10
-    c-=1
-if s==temp:
-    print("True")
-else:
-    print("False")
+print(is_disarium(n))
